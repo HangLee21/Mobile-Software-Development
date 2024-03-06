a79ef7e0aa5a0d2ad4328135d7c1d3f4f3ccd765
chl <3102727009@qq.com>
2024-03-06 22:21:46 +0800

    个人页面的子页面，验证码

--- forum/lib/components/accountpagecard.dart
+++ forum/lib/components/accountpagecard.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:forum/pages/favorite.dart';
+import 'package:forum/pages/history.dart';
+import 'package:forum/pages/settings.dart';
+import 'package:forum/pages/starlist.dart';
+import 'package:forum/pages/worklist.dart';
 import 'package:forum/theme/theme_data.dart';
 
 
@@ -17,18 +22,43 @@ Map colorDict =  {
   '设置': Colors.grey,
 };
 
+Map pageDict = {
+  '收藏': StarList(),
+  '我的创作': WorkList(),
+  '我的关注': FavoriteList(),
+  '历史记录': History(),
+  '设置': Settings(),
+};
+
+
 class AccountPageCard extends StatelessWidget{
   final String name;
   const AccountPageCard(this.name,{super.key});
   @override
   Widget build(BuildContext context){
-
+    void goto(String page){
+        Navigator.push(
+            context,
+            PageRouteBuilder(
+            transitionDuration: Duration(milliseconds: 300),
+              pageBuilder: (_, __, ___) => pageDict[name],
+              transitionsBuilder: (_, animation, __, child) {
+                return SlideTransition(
+                  position: Tween<Offset>(
+                  begin: Offset(1.0, 0.0),
+                  end: Offset.zero,
+                  ).animate(animation),
+                  child: child,
+                );
+              }
+            ));
+    }
     return Card(
       child: Container(
           height: 70,
           child: InkWell(
             onTap: (){
-
+              goto(name);
             },
             child: Row(
               children: [
--- forum/lib/components/content_card.dart
+++ forum/lib/components/content_card.dart
@@ -28,64 +28,113 @@ class ContentCard extends StatelessWidget {
         constraints: BoxConstraints(
           maxHeight: 300.0, // 设置最大高度
         ),
-        child: Card(
-          // unless you need it.
-          clipBehavior: Clip.hardEdge,
-          child: InkWell(
-            splashColor: Colors.blue.withAlpha(30),
-            onTap: () {
-              if(this.url != null){
-                Navigator.of(context)
-                    .popUntil((route) => route.settings.name == '/');
-                Navigator.of(context).restorablePushNamed(url!);
-              }
-            },
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.album),
-                  title: Text(
+        child: GestureDetector(
+          onLongPress: (){
+            _showPopupMenu(context);
+          },
+          child: Card(
+            // unless you need it.
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                if(this.url != null){
+                  Navigator.of(context)
+                      .popUntil((route) => route.settings.name == '/');
+                  Navigator.of(context).restorablePushNamed(url!);
+                }
+              },
+
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.album),
+                    title: Text(
                       card_title,
                       overflow: TextOverflow.ellipsis, // 设置溢出时显示省略号
                       maxLines: 2,
-                  ),
-                  subtitle: Text(
+                    ),
+                    subtitle: Text(
                       card_content,
                       overflow: TextOverflow.ellipsis, // 设置溢出时显示省略号
                       maxLines: 2, // 设置最大行数
                     ),
-                ),
-                if(media_urls != null)
-                  SizedBox(
-                    height: 200.0,
-                    child: CarouselDemo(fileNames: media_urls,),
-                    // child: SingleChildScrollView(
-                    //   scrollDirection: Axis.horizontal,
-                    //   child: Row(
-                    //     children: List.generate(
-                    //       this.media_urls!.length,
-                    //           (index) => Container(
-                    //         margin: EdgeInsets.all(8.0),
-                    //         child: FadeInImage(
-                    //           image: AssetImage(
-                    //             this.media_urls![index],
-                    //           ),
-                    //           placeholder: MemoryImage(kTransparentImage),
-                    //           fit: BoxFit.cover,
-                    //           fadeInDuration: entranceAnimationDuration,
-                    //         ),
-                    //       ),
-                    //     ),
-                    //   ),// 设置水平滚动.builder(
-                    // ),
                   ),
+                  if(media_urls != null)
+                    SizedBox(
+                      height: 200.0,
+                      child: CarouselDemo(fileNames: media_urls,),
+                      // child: SingleChildScrollView(
+                      //   scrollDirection: Axis.horizontal,
+                      //   child: Row(
+                      //     children: List.generate(
+                      //       this.media_urls!.length,
+                      //           (index) => Container(
+                      //         margin: EdgeInsets.all(8.0),
+                      //         child: FadeInImage(
+                      //           image: AssetImage(
+                      //             this.media_urls![index],
+                      //           ),
+                      //           placeholder: MemoryImage(kTransparentImage),
+                      //           fit: BoxFit.cover,
+                      //           fadeInDuration: entranceAnimationDuration,
+                      //         ),
+                      //       ),
+                      //     ),
+                      //   ),// 设置水平滚动.builder(
+                      // ),
+                    ),
 
-              ],
+                ],
+              ),
             ),
           ),
-        ),
+        )
+
       )
+    );
+  }
+
+  void _showPopupMenu(BuildContext context) {
+    final RenderBox overlay = Overlay.of(context)!.context.findRenderObject() as RenderBox;
+
+    final RenderBox cardBox = context.findRenderObject() as RenderBox;
+
+    final ThemeData theme = Theme.of(context);
+
+    final RelativeRect position = RelativeRect.fromLTRB(
+      MediaQuery.of(context).size.width - 100, // 从屏幕右侧向左偏移100像素
+      cardBox.localToGlobal(cardBox.size.bottomRight(Offset.zero), ancestor: overlay).dy, // 与卡片底部对齐
+      MediaQuery.of(context).size.width,
+      overlay.size.height,
+    );
+
+    showMenu(
+      context: context,
+      position: position,
+      items: [
+        PopupMenuItem(
+          child: ListTile(
+            leading: Icon(Icons.delete),
+            title: Text('删除'),
+            onTap: () {
+              // 处理删除操作
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        PopupMenuItem(
+          child: ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text('收藏'),
+            onTap: () {
+              // 处理收藏操作
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
